fccedc2b35e8b4b4ef6d8169d3fefa31c4f945f5
Jothinayagan <v.i.jothinayagan.307@gmail.com>
2022-03-04 10:27:42 +0530

    feat: update, delete user service

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { verifyToken } from "../middleware/verifyToken";
-import { getUsers, getUser } from "../services/user.service";
+import { getUsers, getUser, updateUser, deleteAllUsers, deleteUser } from "../services/user.service";
 
 const userRouter = Router();
 
@@ -8,5 +8,8 @@ userRouter.use(verifyToken);
 
 userRouter.get("/getUsers", getUsers);
 userRouter.get("/getUser", getUser);
+userRouter.post("/updateUser", updateUser);
+userRouter.delete("/deleteAllUsers", deleteAllUsers);
+userRouter.delete("/deleteUser", deleteUser);
 
 export default userRouter;
--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,14 +1,16 @@
 import { Request, Response } from "express";
+import { isEmpty } from "lodash";
 import logger from "../utilies/logger";
 import { database } from "../model";
 import { User } from "../interface/interface";
-import { failureResponse, successResponseWithData } from "../utilies/helpers";
+import message from "../utilies/message.json";
+import { failureResponse, successResponse, successResponseWithData } from "../utilies/helpers";
 
 const user = database.user;
 
 export async function getUsers(_req: Request, res: Response) {
     try {
-        let users: User[] = await user.findAll({ attributes: ["id", "name", "email"] });
+        const users: User[] = await user.findAll({ attributes: ["id", "name", "email"] });
         return successResponseWithData(res, users);
     } catch (error: any) {
         logger.error(`getUser: ${error.message}`);
@@ -20,7 +22,7 @@ export async function getUser(req: Request, res: Response) {
     try {
         const userId: string = req.body.id;
 
-        let singleUser: User = await user.findOne({ where: { id: userId }, attributes: ["id", "name", "email"] });
+        const singleUser: User = await user.findOne({ where: { id: userId }, attributes: ["id", "name", "email"] });
         return successResponseWithData(res, singleUser);
     } catch (error: any) {
         logger.error(`get single user: ${error.message}`);
@@ -28,27 +30,45 @@ export async function getUser(req: Request, res: Response) {
     }
 }
 
-export async function updateUser(_req: Request, res: Response) {
+export async function updateUser(req: Request, res: Response) {
     try {
-        // TODO: Update user by id
+        const { id, name, email } = req.body;
+
+        if (isEmpty(name) && isEmpty(email)) return successResponse(res, message.no_update_user);
+
+        let data = {
+            ...(!isEmpty(name) && { name }),
+            ...(!isEmpty(email) && { email }),
+        };
+
+        await user.update(data, { where: { id } });
+        return successResponse(res, message.update_user);
     } catch (error: any) {
         logger.error(`update user: ${error.message}`);
         return failureResponse(res, error.message);
     }
 }
 
-export async function deleteUsers(_req: Request, res: Response) {
+export async function deleteAllUsers(_req: Request, res: Response) {
     try {
-        // TODO: delete all user
+        const count = await user.destroy({ where: {} });
+
+        if (count === 0) return successResponse(res, message.no_all_user_delete);
+        return successResponse(res, message.all_user_delete);
     } catch (error: any) {
         logger.error(`delete all user: ${error.message}`);
         return failureResponse(res, error.message);
     }
 }
 
-export async function deleteUser(_req: Request, res: Response) {
+export async function deleteUser(req: Request, res: Response) {
     try {
-        // TODO: delete single user by id
+        const { id } = req.body;
+
+        const count = await user.destroy({ where: { id } });
+
+        if (count === 1) return successResponse(res, message.user_delete);
+        return successResponse(res, message.no_user_delete);
     } catch (error: any) {
         logger.error(`delete single user: ${error.message}`);
         return failureResponse(res, error.message);
